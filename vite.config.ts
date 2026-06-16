89e70ed044ccf3388402e90134b6f1db8e424da3
DESKTOP-1VIEEVT\user <pandalee1147@gmail.com>
2026-06-16 20:12:04 +0900

    chore: base URL을 다시 /AI_coding_page/로 복구

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/minjae0403.github.io/',
+  base: '/AI_coding_page/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
